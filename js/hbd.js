38765aa9ba6cb5ef288ee2299b4fb3f761262dd5
Moaz Alsabouh <ibrahaem005@gmail.com>
2022-08-14 05:33:25 +0300

    add f

--- js/hbd.js
+++ js/hbd.js
@@ -9,7 +9,6 @@ let countDownEnd = new Date("May 8, 2023 00:00:00").getTime();
 let countDown = setInterval(() => {
   // now time 
   let nowTime = new Date().getTime();
-
   // rem time
   let remCountdown = countDownEnd - nowTime;
   // let remCountdown = nowTime - countDown;
@@ -30,4 +29,29 @@ let countDown = setInterval(() => {
     // minets.textContent = minet;
     // secends.textContent =  secend;
   }
+}, 1000);
+
+// 
+let sec = document.querySelector(".sec"),
+  min = document.querySelector(".min"),
+  hou = document.querySelector(".hou"),
+  day = document.querySelector(".day"),
+  wee = document.querySelector(".wee"),
+  mon = document.querySelector(".mon"),
+  yea = document.querySelector(".yea");
+  // 
+let bd = new Date("May 8, 2003 00:00:00");
+
+
+
+let age = setInterval (() => {
+  let timeNow = new Date().getTime(),
+  sAge = timeNow - bd;
+  sec.textContent = Math.floor(sAge / 1000 );
+   min.textContent = Math.floor(sAge / (1000 * 60));
+   hou.textContent = Math.floor(sAge / (1000 * 60 * 60));
+   day.textContent = Math.floor(sAge / (1000 * 60 * 60 * 24));
+   wee.textContent = Math.floor(sAge / (1000 * 60 * 60 * 24 * 7));
+   mon.textContent = Math.floor(sAge / (1000 * 60 * 60 * 24 * 30));
+   yea.textContent = Math.floor(sAge / (1000 * 60 * 60 * 24 * 364.25));
 }, 1000);
